1a3071bbcf58aaf1e86fd932bd9397c296720624
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-09-21 10:49:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//var life = 100; // This is my life NUMBER (Integer/Float) 
+
+//life = 50; //This will set the life variable to 50
+//life = life - 10; //This will subtract the life variable by 10.
+//life = life/10; //This will divide the life variable by 10.
+
+//var name = "Dev Ed"; //This is he name displayed in the game STRING
+//var checkout = false; //BOOLEAN value. True or False.
+//var box; //Undefined Variable
+//var box = null; //Undefined Variable
+
+//const life = 100; //After being declared it can no longer be changed
+
+let life = 100; //want to create a variable and change the value
+
+console.log(life);
