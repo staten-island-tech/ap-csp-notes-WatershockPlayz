b01cc0d2ee0c6ea628dcccb6714142f2f6271c44
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-09-22 23:51:25 -0400

    commit

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-//var life = 100; // This is my life NUMBER (Integer/Float) 
 
-//life = 50; //This will set the life variable to 50
-//life = life - 10; //This will subtract the life variable by 10.
-//life = life/10; //This will divide the life variable by 10.
+//////////////////////Javascript Functions and Parameters///////////////////////
 
-//var name = "Dev Ed"; //This is he name displayed in the game STRING
-//var checkout = false; //BOOLEAN value. True or False.
-//var box; //Undefined Variable
-//var box = null; //Undefined Variable
+const names = "Dev Ed"; //Const Variable in global scope , see Lesson 1 (Or not, it's pretty easy to remember)
+const youtuber =  "Traversy Media";
 
-//const life = 100; //After being declared it can no longer be changed
+function adder(num1, num2) { //Local variables created in paranthesis
+    console.log(num1 + num2);
+}
 
-let life = 100; //want to create a variable and change the value
+function logger() { //A function is a series of commands that can be called when the function is called with copying and pasting
+    //const name = "Dev Ed"; //Only available in the function, local scpe
+    console.log("Party Time");
+    console.log("Party Time");
+    console.log("Party Time");
+    console.log("Party Time");
+}
 
-console.log(life);
+function toUpper(text) { //Editing text to become Upper case
+    const upperCased = text.toUpperCase(); //Creates a new variable and makes it Upper case version of text.
+    console.log(upperCased);
+}
+
+toUpper(names); //Calling the function
+toUpper(youtuber); //Calling the function
+//adder(5, 10);
+
+//const toUpper = () =>{ //Shortest way to write the function
+
+//}
+
+
+
+
+
+////////////////////Javascript String Concatenation/////////////////////////////
+
+const myAge = 16; //Variable set to value of 16
+const yourAge = 24;
+
+console.log(myAge + yourAge); //Adds ages together
+
+const name = "Melvin";
+
+const combined = name + myAge;
+
+
+console.log(combined); //Will always be a String
+
+console.log('Hello my name is ' + 'Ed'); //Adds the Strings together
+console.log('Hello my name it\'s Ed'); //Fix issues
+
+console.log(`Hello it's me and my name is ${name}` ); //How to use javascript in a string is to use: ${}
