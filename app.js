52362f389bde0273e26bd02bf894bc39068461bb
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-09-23 21:04:51 -0400

    commit

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ function toUpper(text) { //Editing text to become Upper case
     console.log(upperCased);
 }
 
-toUpper(names); //Calling the function
-toUpper(youtuber); //Calling the function
+//toUpper(names); //Calling the function
+//toUpper(youtuber); //Calling the function
 //adder(5, 10);
 
 //const toUpper = () =>{ //Shortest way to write the function
@@ -38,16 +38,48 @@ toUpper(youtuber); //Calling the function
 const myAge = 16; //Variable set to value of 16
 const yourAge = 24;
 
-console.log(myAge + yourAge); //Adds ages together
+//console.log(myAge + yourAge); //Adds ages together
 
 const name = "Melvin";
 
 const combined = name + myAge;
 
 
-console.log(combined); //Will always be a String
+//console.log(combined); //Will always be a String
 
-console.log('Hello my name is ' + 'Ed'); //Adds the Strings together
-console.log('Hello my name it\'s Ed'); //Fix issues
+//console.log('Hello my name is ' + 'Ed'); //Adds the Strings together
+//console.log('Hello my name it\'s Ed'); //Fix issues
 
-console.log(`Hello it's me and my name is ${name}` ); //How to use javascript in a string is to use: ${}
+//console.log(`Hello it's me and my name is ${name}` ); //How to use javascript in a string is to use: ${}
+
+//////////////////////////MATH////////////////////////////////
+
+const num1 = 100;
+const num2 = 100;
+let val;
+
+val = num1 + num2; //Simple Math with numbers ADD
+val = num1 - num2; //Simple Math with numbers SUBTRACT
+val = num1 * num2; //Simple Math with numbers MULTIPLY
+val = num1 / num2; //Simple Math with numbers DIVIDE
+val = num1 % num2; //Simple Math with numbers PERCENT
+
+//Math Object
+val = Math.PI; //PI
+val = Math.E; //e
+val = Math.round(2.8); //round to the nearest integer
+val = Math.ceil(2.4); //round up
+val = Math.floor(2.8); //round down
+val = Math.sqrt(64); //Squar Root
+val = Math.abs(-3); //Absolute value
+val = Math.pow(8, 2); //Power
+val = Math.min(100, 80, 70, 73, -10, 8, 23, 45, 93); //Minimum value from a list
+val = Math.max(100, 80, 70, 73, -10, 8, 23, 45, 93); //Maximum value from a list
+val = Math.random(); //Random decimal number
+
+val = Math.random() * 20; //Random decimal number that is multiplied by 1
+
+val = Math.floor(Math.random() * 20 + 1); //Random number rounded down above 0
+
+
+console.log(val);
